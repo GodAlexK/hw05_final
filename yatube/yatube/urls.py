dedd6410b074990ce449d3e25981bb0a6d4c40f2
Alexeu Korenkov <kenlexa97@gmail.com>
2023-05-10 21:17:23 +0300

    Bad Last version

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -32,6 +31,8 @@ handler403 = 'core.views.csrf_failure'
 handler404 = 'core.views.page_not_found'
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
